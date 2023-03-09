086c80c445f055436a52c4230af13e9505352e18
Igor Ilić <ilicigor@outlook.com>
2023-03-09 22:35:56 +0100

    feat: added prettier (#1)

--- app/root.tsx
+++ app/root.tsx
@@ -1,24 +1,24 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
 
-import styles from "./tailwind.css";
+import styles from './tailwind.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1'
+})
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: "https://rsms.me/inter/inter.css"},
-  { rel: "stylesheet", href: styles }
+  { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+  { rel: 'stylesheet', href: styles }
 ]
 
 export default function App() {
@@ -35,5 +35,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
